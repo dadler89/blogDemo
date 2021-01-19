615d7904f6acf260ca137b23cf96df6b38c2d905
Alex Adler <69403909+dadler89@users.noreply.github.com>
2021-01-19 06:45:18 -0600

    Decent stopping point

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-
+const blogPost = []
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(express.static("public"));
 
 app.get("/", (request, response) => {
 
-  
+  console.log(blogPost);
   response.render("home", {statement: homeStartingContent})
 })
 
@@ -30,6 +30,8 @@ app.get("/", (request, response) => {
 
 
 app.get("/about", (req, res) => {
+
+  
 
   res.render("about", {statement: aboutContent})
 });
@@ -49,9 +51,15 @@ app.get("/compose", (req, res) => {
 
 });
 
-app.post("/compose" , (req, res) => {
- 
-  console.log(req.body.postTitle)
+app.post("/compose" , (req, res) => { 
+  const post = {
+    title: req.body.postTile,
+    content: req.body.postBody
+  }
+  blogPost.push(post)
+  res.redirect("/")
+
+
 })
 
 app.listen(3000, function() {
